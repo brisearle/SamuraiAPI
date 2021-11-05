ef0b257e74b1a52a19b91f28efbb30f4cee47967
Brian Searle <bsearle@silvergate.com>
2021-11-05 12:24:34 -0700

    Module 8

--- SamuraiApp.Data/SamuraiContextNoTracking.cs
+++ SamuraiApp.Data/SamuraiContextNoTracking.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿/*using Microsoft.EntityFrameworkCore;
 
 namespace SamuraiApp.Data
 {
@@ -9,4 +9,4 @@ namespace SamuraiApp.Data
             base.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
     }
-}
+}*/
